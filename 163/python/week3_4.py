7f39294a511eb5395bb182616b1952919b66ea3f
Wenlong Wang <lonwon@126.com>
2014-12-17 22:27:17 +0800

    update

--- 163/python/week3_4.py
+++ 163/python/week3_4.py
@@ -9,24 +9,30 @@ def isPrim(n):
 	return ret	
 
 def isLoopPrim(n):
+	print "n:",n
 	ret = 1
-	deg = int(math.floor(math.log10(num))+1)
-	for i in range(1,deg+1):
-		t = math.pow(10,deg-i+1)
-		left = int(n/t)	
-		right = n % t
-		new = right*math.pow(10,i)+left
-		print "new num:",new
-		if isPrim(new) == 0:
-			ret = 0
-			break
+	if isPrim(n) == 0:
+		return 0
+	deg = int(math.floor(math.log10(n))+1)
+	print "deg",deg
+	if deg >1:
+		print "x",n
+		for i in range(1,deg+1):
+			t = math.pow(10,deg-i+1)
+			left = int(n/t)	
+			right = n % t
+			new = right*math.pow(10,i)+left
+			print "new num:",new
+			if isPrim(new) == 0:
+				ret = 0
+				break
 	return ret
 
+
 num = int(raw_input())
-print num
 sum = 0
 for a in range(2,num):
-	if isPrim(a) == 1:
-		if isLoopPrim(a) == 1:
-			sum += 1
-print sum	
+	if isLoopPrim(a) == 1:
+		sum += 1
+		print "sum:",sum
+print "total sum:",sum	
